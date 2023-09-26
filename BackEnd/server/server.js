e8ea8b7c0d12aa4aaa879db37382a375180680cd
SerialPenguin <larsson.alexander@live.se>
2023-09-26 15:31:08 +0200

    Added simple backend to fetch movies

---
+++ BackEnd/server/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.error(`MongoDB connection error: ${err}`);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Define your routes to fetch data directly from the "Movies" collection
+app.get('/movies', async (req, res) => {
+  try {
+    const collection = db.collection('Movies'); // Access the "Movies" collection
+    const movies = await collection.find({}).toArray();
+    
+    if (movies.length === 0) {
+      res.status(404).json({ message: 'No movies found' });
+    } else {
+      res.json(movies);
+    }
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+
+

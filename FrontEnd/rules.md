3d4ed6c2be637c5005391872e8a36094a23e828b
SerialPenguin <larsson.alexander@live.se>
2023-09-27 11:29:46 +0200

    Added a simple backend to fetch movies.

---
+++ FrontEnd/rules.md
@@ -0,0 +1,32 @@
+### `Branches`
+
+Ny branch för varje komponent.
+Pull request görs med minst halva gruppen.
+
+### `Arbetstider`
+
+08.30 - 16.30 Arbetstid (standard).
+09.00 Morgonmöte & Standup.
+12.00 Lunch (standard).
+13.15 Avstämningsmöte.
+
+### `Namnkonvention`
+
+Standard: camelCase.
+Komponenter: PascalCase.
+Classname: kebab-case.
+
+### `Mappstruktur`
+
+Komponenter: "src/Components/ExampleComponent/ExampleComponent.js, ExampleComponent.test.js, ExampleComponent.css"
+
+Custom hooks: Egen mapp "Hooks".
+
+Övriga komponenter som inte platsar i Components-mappen läggs i "src/abstract".
+
+### `Övrigt`
+
+Funktioner deklareras med "function".
+Arrow-functions tillåtet i callbacks.
+
+Commits görs hellre flera små än få stora.
---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    console.log('Fetching movies...');
+
+    fetch('/movies')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json(); 
+      })
+      .then((data) => {
+        console.log('Fetched movies:', data);
+        setMovies(data); 
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  console.log('Rendered with movies:', movies); 
+
+  return (
+    <div className="App">
+      <h1>Movies</h1>
+      <div className="movie-grid">
+        {movies.length === 0 ? (
+          <p>No movies found</p>
+        ) : (
+          movies.map((movie) => (
+            <div key={movie._id} className="movie-card">
+              <h2>{movie.title}</h2>
+              <p>{movie.description}</p>
+              <img src={movie.images} alt={movie.title}></img>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;

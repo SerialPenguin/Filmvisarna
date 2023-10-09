31e3a3c168472b95d47e9976d8f6d03b588096b6
MadeleneFasth <113009672+MadeleneFasth@users.noreply.github.com>
2023-10-09 13:39:09 +0200

    test

--- BackEnd/server/server.js
+++ BackEnd/server/server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const movieRoutes = require('./routes/movieRoutes');
-const screeningRoutes = require('./routes/screeningRoutes');
-require('./config/db');
+const express = require("express");
+const movieRoutes = require("./routes/movieRoutes");
+const screeningRoutes = require("./routes/screeningRoutes");
+require("./config/db");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/api/movies', movieRoutes);
-app.use('/api/screenings', screeningRoutes);
+app.use("/api/movies", movieRoutes);
+app.use("/api/screenings", screeningRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

454499fca45ee948254da839d5ae1e829f5afed6
RobbanBroberg <lafarchetamustatche@outlook.com>
2023-10-12 18:32:53 +0200

    restrictions on rows and seats implemented

--- BackEnd/server/controllers/bookingController.js
+++ BackEnd/server/controllers/bookingController.js
@@ -7,11 +7,50 @@ import authService from '../service/authService.js';
 
 export const bookSeat = async (req, res) => {
   try {
-    const { screeningId, salonId, seat } = req.body;
-    console.log("Request body:", req.body);
+    const { screeningId, salonId, seat} = req.body;
 
     const screening = await Screening.findById(new mongoose.Types.ObjectId(screeningId));
-    console.log("Find Screening:", screening);
+    // console.log("Find Screening:", screening);
+
+    try {
+      const collection = mongoose.connection.collection('seats');
+      const seats = await collection.find({ "_id": screening.salonId }).toArray();
+
+      let maxRows;
+      let minRows;
+      
+      if(seats[0].capacity === 55) {
+        maxRows = 6;
+        minRows = 1;
+
+        if(seat.rowNumber > maxRows || seat.rowNumber < minRows) {
+          return res.status(400).json({ msg: "Row does not exist."})
+        }
+        
+        const availableSeats = seats[0].rows[seat.rowNumber - 1].seats;
+        
+        if(!availableSeats.includes(seat.seatNumber)) {
+          return res.status(400).json({ msg: "Seat does not exist on chosen row." })
+        }
+
+      }else if(seats[0].capacity === 81) {
+        maxRows = 8;
+        minRows = 1;
+
+        if(seat.rowNumber > maxRows || seat.rowNumber < minRows) {
+          return res.status(400).json({ msg: "Row does not exist."})
+        }
+        
+        const availableSeats = seats[0].rows[seat.rowNumber - 1].seats;
+        
+        if(!availableSeats.includes(seat.seatNumber)) {
+          return res.status(400).json({ msg: "Seat does not exist on chosen row." })
+        }
+      }
+
+    }catch(err) {
+        console.log(err)
+    }
 
     const existingBooking = await Booking.findOne({
       screeningId,
@@ -20,7 +59,6 @@ export const bookSeat = async (req, res) => {
     });
 
     if (existingBooking) {
-      console.log("Booking attempt for already booked seat:", seat);
       return res.status(400).json({ error: 'Seat is already booked' });
     }
 
@@ -77,9 +115,6 @@ export const bookSeat = async (req, res) => {
       });
 
       await newBooking.save();
-      console.log("Booking saved:", newBooking);
-
-      console.log("ScreeningId Type and Value:", typeof screeningId, screeningId);
 
       const user = await User.findOne({_id: userInfo.id});
 
@@ -94,14 +129,10 @@ export const bookSeat = async (req, res) => {
         { $push: { bookedSeats: seat } }
       );
 
-      console.log("Screening update Response:", screeningUpdateResponse);
-
       // Fetch updated screening for logging
       const updatedScreening = await Screening.findOne(
         { _id: new mongoose.Types.ObjectId(screeningId) }
       );
-
-      console.log("Updated Screening:", updatedScreening);
 
       res.status(201).json({ message: 'Booking created!', booking: newBooking });
     }

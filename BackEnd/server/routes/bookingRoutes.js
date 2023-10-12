1c72caec463d9609f35e15d9dd7bfffb41f366af
SerialPenguin <larsson.alexander@live.se>
2023-10-12 13:56:15 +0200

    Added a fetch for bookings

--- BackEnd/server/routes/bookingRoutes.js
+++ BackEnd/server/routes/bookingRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { bookSeat } from '../controllers/bookingController.js';
+import { getAllBookings } from '../controllers/displayBookingControllers.js';
 
 const router = express.Router();
 
 router.patch('/', bookSeat);
+router.get('/', getAllBookings);
 
 export default router;

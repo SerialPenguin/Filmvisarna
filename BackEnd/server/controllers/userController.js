4e14e3581dfa4bdb4898888e32faa5dc25e01f77
PontusEkstrom02 <113009598+PontusEkstrom02@users.noreply.github.com>
2023-10-16 13:58:07 +0200

    Made getUserInfo, deleteBooking and logoutUser

--- BackEnd/server/controllers/userController.js
+++ BackEnd/server/controllers/userController.js
@@ -48,3 +48,65 @@ export const login = async (req, res) => {
   const token = jwt.sign({ id: user._id }, secretKey);
   res.json({ token });
 };
+
+export const getUserInfo = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const deleteBooking = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const bookingId = req.params.bookingId;
+
+    // Find the user by their ID
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Check if the booking exists in the user's bookingHistory
+    const bookingIndex = user.bookingHistory.indexOf(bookingId);
+
+    if (bookingIndex === -1) {
+      return res.status(404).json({ error: 'Booking not found in user history' });
+    }
+
+    // Remove the booking from the user's bookingHistory
+    user.bookingHistory.splice(bookingIndex, 1);
+
+    await user.save();
+
+    res.json({ message: 'Booking deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting booking:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+export const logoutUser = (req, res) => {
+  const token = req.headers.authorization; // Assuming the token is passed in the Authorization header
+
+  // Check if the token is already invalidated
+  if (invalidatedTokens.has(token)) {
+    return res.status(401).json({ error: 'Token already invalidated' });
+  }
+
+  // Add the token to the blacklist
+  invalidatedTokens.add(token);
+
+  res.json({ message: 'User logged out successfully' });
+};

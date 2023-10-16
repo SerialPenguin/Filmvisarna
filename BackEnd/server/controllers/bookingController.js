3ec6b563bfc6292dfb35de456ec5ef7e7a474471
RobbanBroberg <lafarchetamustatche@outlook.com>
2023-10-16 10:50:44 +0200

    moved booking number generator function out of bookingController

--- BackEnd/server/controllers/bookingController.js
+++ BackEnd/server/controllers/bookingController.js
@@ -3,6 +3,8 @@ import Booking from '../models/bookingModel.js';
 import Screening from '../models/screeningModel.js';
 import User from "../models/userModel.js";
 import authService from '../service/authService.js';
+import generatorService from '../service/generatorService.js.js';
+
 
 
 export const bookSeat = async (req, res) => {
@@ -62,75 +64,55 @@ export const bookSeat = async (req, res) => {
       return res.status(400).json({ error: 'Seat is already booked' });
     }
 
-    const randomChars = []
-
-    let result = generateBookingNumber();
-
-    function generateBookingNumber() {
-      for(let i = 0; i < 6; i++) {
-        let random;
-  
-        if(i <= 2){
-          random = Math.floor(Math.random() * (90 - 65) + 65);
-        }
-        else {
-          random = Math.floor(Math.random() * 10);
-        }
-  
-        if(random < 10 || random > 64) {
-          if(random > 64) random = String.fromCharCode(random)
-          randomChars.push(random);
-        }else i--;
-      }
-
-      let result = randomChars.join('');
-      console.log("Booking number:" + result);
-      return result;
-    }
-
+    let bookingNumber = generatorService.generateBookingNumber();
+    
     let existingBookingNumber = await Booking.findOne({
-      bookingNumber: result
+      bookingNumber: bookingNumber
     })
-
+  
     if(existingBookingNumber) {
-      result = generateBookingNumber();
-    }else {
-
-      const authHeader = req.headers["authorization"];
-
-      const userInfo = await authService.verifyJwt(authHeader);
-
-      const userEmail = email ? email : "no email";
-
-      const newBooking = new Booking({
-        screeningId,
-        salonId,
-        seat,
-        bookedBy: {
-          user: userInfo.id ? userInfo.id : userInfo,
-          email: userEmail,
-        },
-        bookingNumber: result,
-        ticketTypeId,
-      });
-
-      await newBooking.save();
-
-      if(userInfo.id) {
-        const user = await User.findOne({_id: userInfo.id});
-
-        const updateUserBookingHistory = await user.updateOne(
-          { $push: {bookingHistory: screeningId }},
-        );
-      }
-
-      const screeningUpdateResponse = await Screening.updateOne(
-        { _id: new mongoose.Types.ObjectId(screeningId) },
-        { $push: { bookedSeats: seat } }
-      );
-
-      res.status(201).json({ message: 'Booking created!', booking: newBooking });
+      bookingNumber = generatorService.generateBookingNumber();
     }
+
+    const authHeader = req.headers["authorization"];
+
+    const userInfo = await authService.verifyJwt(authHeader);
+
+    const userEmail = email ? email : "no email";
+
+    const newBooking = new Booking({
+      screeningId,
+      salonId,
+      seat,
+      bookedBy: {
+        user: userInfo.id ? userInfo.id : userInfo,
+        email: userEmail,
+      },
+      bookingNumber: bookingNumber,
+      ticketTypeId,
+    });
+
+    await newBooking.save();
+
+    if(userInfo.id) {
+      const user = await User.findOne({_id: userInfo.id});
+
+      const booking = await Booking.findOne({
+        screeningId,
+        "bookingNumber": bookingNumber,
+      })
+
+      const updateUserBookingHistory = await user.updateOne(
+        { $push: {bookingHistory: booking._id}},
+      );
+    }
+
+    const screeningUpdateResponse = await Screening.updateOne(
+      { _id: new mongoose.Types.ObjectId(screeningId) },
+      { $push: { bookedSeats: seat } }
+    );
+
+    res.status(201).json({ message: 'Booking created!', booking: newBooking });
   } catch (error) {
     console.error('Error creating booking:', error);
     res.status(500).json({ error: 'Internal server error' });

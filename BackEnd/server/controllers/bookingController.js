6453c9524a6a2cdeb31b30a689a3da242b4d9228
Erica Eklund <erica@iMac-som-tillhor-Erica.local>
2023-10-17 12:23:02 +0200

    implemented mailer into bookings - send email in body - sends email

--- BackEnd/server/controllers/bookingController.js
+++ BackEnd/server/controllers/bookingController.js
@@ -4,6 +4,7 @@ import Screening from '../models/screeningModel.js';
 import User from "../models/userModel.js";
 import authService from '../service/authService.js';
 import generatorService from '../service/generatorService.js.js';
+import sendConfirmation from '../service/mailService.js';
 
 
 export const bookSeat = async (req, res) => {
@@ -14,23 +15,23 @@ export const bookSeat = async (req, res) => {
     const collection = mongoose.connection.collection('seats');
     const salon = await collection.find({ "_id": screening.salonId }).toArray();
     const movie = mongoose.connection.collection('movies');
-    const movieInfo = await movie.find({"_id": screening.movieId}).toArray();
+    const movieInfo = await movie.find({ "_id": screening.movieId }).toArray();
 
-    if(movieInfo[0].age >= 15) {
-      if(ticketTypeId.includes("65279fcd702eef67b26ef3c4")) {
-        return res.status(405).json({ msg: "Ticket for children is unavailable, age restriction is applied!"});
+    if (movieInfo[0].age >= 15) {
+      if (ticketTypeId.includes("65279fcd702eef67b26ef3c4")) {
+        return res.status(405).json({ msg: "Ticket for children is unavailable, age restriction is applied!" });
       }
     }
-  
+
     // Controlling that seat number from body is within range of salon
-    for(let seat of seats) {
+    for (let seat of seats) {
       if (salon[0].capacity === 55) {
-        if(seat > 55 || seat < 1) {
-          return res.status(400).json({ msg: `Seat doesnt exist in the ${salon[0].name}`});
+        if (seat > 55 || seat < 1) {
+          return res.status(400).json({ msg: `Seat doesnt exist in the ${salon[0].name}` });
         }
       } else if (salon[0].capacity === 81) {
-        if(seat > 81 || seat < 1) {
-          return res.status(400).json({ msg: `Seat doesnt exist in the ${salon[0].name}`});
+        if (seat > 81 || seat < 1) {
+          return res.status(400).json({ msg: `Seat doesnt exist in the ${salon[0].name}` });
         }
       }
 
@@ -44,17 +45,17 @@ export const bookSeat = async (req, res) => {
     }
 
     let bookingNumber = generatorService.generateBookingNumber();
-    
+
     let existingBookingNumber = await Booking.findOne({
       bookingNumber: bookingNumber
     })
-  
-    if(existingBookingNumber) {
+
+    if (existingBookingNumber) {
       bookingNumber = generatorService.generateBookingNumber();
     }
 
     const authHeader = req.headers["authorization"];
-
+    console.log(email);
     const userId = await authService.verifyJwt(authHeader);
     const userEmail = email ? email : "no email";
 
@@ -73,19 +74,22 @@ export const bookSeat = async (req, res) => {
     await newBooking.save();
     res.status(201).json({ message: 'Booking created!', booking: newBooking });
 
-    
+    sendConfirmation(bookingNumber, email);
+
     await Screening.updateOne(
       { _id: new mongoose.Types.ObjectId(screeningId) },
       { $push: { bookings: newBooking._id } }
     );
 
-    if(userId) {
-      const user = await User.findOne({_id: userId});
+    if (userId) {
+      const user = await User.findOne({ _id: userId });
 
       const updateUserBookingHistory = await user.updateOne(
-        { $push: {bookingHistory: newBooking._id}},
+        { $push: { bookingHistory: newBooking._id } },
       );
     }
+
+
 
   } catch (error) {
     console.error('Error creating booking:', error);

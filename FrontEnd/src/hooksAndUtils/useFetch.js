7a25ab470e492e517b14c1c1592758e55a600dbe
PontusEkstrom02 <113009598+PontusEkstrom02@users.noreply.github.com>
2023-10-24 11:47:22 +0200

    hooksandutils

---
+++ FrontEnd/src/hooksAndUtils/useFetch.js
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+import { get, post, patch, del } from './fetchUtil';
+
+function FetchHelper(func, stateSetter) {
+  useEffect(() => {
+    (async () => {
+      let result = await func();
+      stateSetter(result);
+    })();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+}
+
+export function useGet(url, stateSetter) {
+    FetchHelper(() => get(url), stateSetter, url, 'get');
+}
+
+export function usePost(url, body, stateSetter) {
+    FetchHelper(() => post(url, body), stateSetter, url, 'post');
+}
+
+export function usePatch(url, body, stateSetter) {
+    FetchHelper(() => patch(url, body), stateSetter, url, 'PATCH')
+}
+
+export function useDelete(url, stateSetter) {
+    FetchHelper(() => del(url), stateSetter, url, 'delete');
+}
+
+/*
+const [products, setProducts] = useState([]);
+useGet('/api/products', setProducts);
+*/

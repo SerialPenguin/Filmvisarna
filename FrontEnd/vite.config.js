fffc7162d923d1f659c745480c84c8f659defa78
MadeleneFasth <113009672+MadeleneFasth@users.noreply.github.com>
2023-10-24 13:26:52 +0200

    profilepage created

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000', // Specify the address of your Express server
+      "/api": {
+        target: "http://localhost:3000", // Specify the address of your Express server
         changeOrigin: true,
         // You can add additional proxy options here if needed
       },

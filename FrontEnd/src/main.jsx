d3bc37dc7a1d03ca17ce1b28a925e92dae79ac0d
RobbanBroberg <lafarchetamustatche@outlook.com>
2023-10-24 13:08:39 +0200

    change router to have outlet

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import App from './App.jsx';
+import Home from './pages/Home.jsx';
+import Booking from './pages/Booking.jsx';
+import './index.css';
+
+const router = createBrowserRouter([{ 
+  path: '/',
+  element: <App />,
+  children: [
+    {path: '/', element: <Home />},
+    {path: '/booking/:screeningId', element: <Booking />},
+  ]
+}])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

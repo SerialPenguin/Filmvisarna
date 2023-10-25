3086c7a52a95f22b639144d38b4a23dfe7c60733
Jonathan Andersson <2jonand@gmail.com>
2023-10-25 23:42:48 +0200

    added login route

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx';
 import Home from './pages/Home.jsx';
 import Booking from './pages/Booking.jsx';
 import Register from './pages/Register.jsx';
+import Login from './pages/Login.jsx';
 import './index.css';
 
 const router = createBrowserRouter([{ 
@@ -13,7 +14,8 @@ const router = createBrowserRouter([{
   children: [
     {path: '/', element: <Home />},
     {path: '/booking/:screeningId', element: <Booking />},
-    {path: '/register', element: <Register />}
+    {path: '/register', element: <Register />},
+    {path: '/login', element: <Login />}
   ]
 }])
 

7ca4e44fe16359061b6adfa05095a31c18a5c57a
SerialPenguin <larsson.alexander@live.se>
2023-10-25 12:25:50 +0200

    Added drop down menu for screening/movie selection along with all the different screenings for that movie.

--- FrontEnd/src/pages/Booking.jsx
+++ FrontEnd/src/pages/Booking.jsx
@@ -1,36 +1,75 @@
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 function Booking() {
   const { screeningId } = useParams();
+  const history = useNavigate();
+  
   const [movie, setMovie] = useState(null);
+  const [movies, setMovies] = useState([]); // For the dropdown
   const [screening, setScreening] = useState(null);
+  const [screenings, setScreenings] = useState([]); // For the dropdown
+  const [selectedMovie, setSelectedMovie] = useState('');
   const [salonLayout, setSalonLayout] = useState(null);
   const [loading, setLoading] = useState(true);
   const [bookedSeats, setBookedSeats] = useState([]);
   const [initialSeatsDataReceived, setInitialSeatsDataReceived] = useState(false);
-  const [previousSeat, setPreviousSeat] = useState(null);  // <-- New state
+  const [previousSeat, setPreviousSeat] = useState(null);
 
-
-    useEffect(() => {
+  // EventSource for live booking updates
+  useEffect(() => {
     const eventSource = new EventSource(`/api/events/${screeningId}`);
-
     eventSource.onmessage = (event) => {
       const updatedBookedSeats = JSON.parse(event.data);
       setBookedSeats(updatedBookedSeats);
       setInitialSeatsDataReceived(true);
     };
-
     eventSource.onerror = (error) => {
       console.error("EventSource failed:", error);
       eventSource.close();
     };
-
-    return () => {
-      eventSource.close();
-    };
+    return () => eventSource.close();
   }, [screeningId]);
 
+  // Fetch movies for dropdown
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch('/api/movies');
+        if (!response.ok) throw new Error('Failed to fetch movies');
+        const data = await response.json();
+        console.log(data); // Check the movies data you get
+        setMovies(data);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    };
+  
+    fetchMovies();
+  }, []);
+
+  // Fetch screenings when a movie is selected
+  useEffect(() => {
+    if (selectedMovie) {
+      fetch(`/api/screenings`) 
+        .then(response => response.json())
+        .then(data => {
+          // Filter the screenings by the selected movieId
+          const filteredScreenings = data.filter(screening => screening.movieId === selectedMovie);
+          setScreenings(filteredScreenings);
+
+          // Redirect to the first screening for the selected movie
+          if (filteredScreenings.length > 0) {
+            history(`/booking/${filteredScreenings[0]._id}`);
+          } else {
+            setScreenings([]);  // No screenings found for selected movie
+          }
+        })
+        .catch(err => console.error('Error fetching screenings:', err));
+    }
+}, [selectedMovie, history]);
+
+  
   const isSeatBooked = (seatNumber) => {
     return bookedSeats.includes(seatNumber);
   };
@@ -46,7 +85,7 @@ function Booking() {
                 body: JSON.stringify({
                     screeningId,
                     seats: [{ seatNumber }],
-                    previousSeat   // <-- Pass the previous seat to the backend
+                    previousSeat   // Pass the previous seat to the backend
                 })
             });
 
@@ -54,7 +93,7 @@ function Booking() {
 
             if (data !== undefined) {
                 setBookedSeats((prevSeats) => [...prevSeats, seatNumber]);
-                setPreviousSeat(seatNumber);  // <-- Set the current seat as the previous one for next time
+                setPreviousSeat(seatNumber);  // Set the current seat as the previous one for next time
                 console.log(`Seat ${seatNumber} is now temporarily reserved.`);
             } else {
                 console.log(data.error);
@@ -107,10 +146,36 @@ function Booking() {
 
   return (
     <div className="App">
-      {loading || !initialSeatsDataReceived ? (
-        <p>Loading...</p>
-      ) : (
-        <>
+    {loading || !initialSeatsDataReceived ? (
+      <p>Loading...</p>
+    ) : (
+      <>
+           <select 
+        style={{ width: '300px', height: '30px' }}
+        value={selectedMovie}
+        onChange={e => setSelectedMovie(e.target.value)}
+      >
+        <option value="">Select a Movie</option>
+        {movies.map(m => (
+          <option key={m._id} value={m._id}>{m.title}</option>
+        ))}
+      </select>
+
+      <select 
+        style={{ width: '300px', height: '30px' }}
+        value={screeningId}
+        onChange={e => {
+          const newScreeningId = e.target.value;
+          history(`/booking/${newScreeningId}`);
+        }}
+      >
+        <option value="">Select a Screening</option>
+        {screenings.map(s => (
+          <option key={s._id} value={s._id}>
+            {new Date(s.startTime).toLocaleDateString()} at {new Date(s.startTime).toLocaleTimeString()}
+          </option>
+        ))}
+      </select>
           <h1>Booking for: {movie?.title}</h1>
           <h2>Director: {movie?.director}</h2>
           <h3>Description: {movie?.description}</h3>

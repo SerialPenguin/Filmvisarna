a8b8fc96aa8a489d62b8518cc658e26e3cba2aa8
RobbanBroberg <lafarchetamustatche@outlook.com>
2023-10-26 13:55:44 +0200

    booking works, some touches needed

--- FrontEnd/src/Components/BookingConfirmationComponent/BookingConfirmation.jsx
+++ FrontEnd/src/Components/BookingConfirmationComponent/BookingConfirmation.jsx
@@ -3,40 +3,37 @@ import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import TicketFront from '../../img/ticketFront.png';
 import TicketBack from '../../img/ticketBack.png';
+import {patch} from '../../hooksAndUtils/fetchUtil';
 
-
-export default function BookingConfirmation() {
+export default function BookingConfirmation(props) {
 
   const navigate = useNavigate();
 
-  const body = {
-    tickets: [{ticketType: "child", quantity: 2}, {ticketType: "adult", quantity: 2}],
-    seats: [{"seatNumber": 1}, {"seatNumber": 2},{"seatNumber": 3},{"seatNumber": 4}],
-  }
-
-  const [ confirmationEmail, setConfirmationEmail ] = useState("");
+  const [ email, setEmail ] = useState("");
   const [ bookingBody, setBookingBody ] = useState({});
   const [ toggleClassName, setToggleClassName] = useState("ticket");
   const [ animationDone, setAnimationDone ] = useState(false);
   const [ bookingNumber, setBookingNumber ] = useState('QWE123');
 
-  //This function is going to take the body with tickets and seats with props
   function handleBookingBody(e) {
-    setConfirmationEmail(e.target.value);
-    setBookingBody({...body, confirmationEmail});
-    console.log("BB: ", bookingBody);
+    setEmail(e.target.value);
+    setBookingBody({...props.body, email});
   }
-
-  console.log("out func: ", toggleClassName)
   
-  function handleSendConfirmation() {
-    // usePatch(('/api/bookings', bookingBody), setBookingNumber )
-    setToggleClassName("ticket-animation");
-    console.log("in func: ", toggleClassName);
-    setTimeout(() => {
-      setAnimationDone(true);
-    }, 3200);
-    
+  async function handleSendConfirmation() {
+
+    const booking = await patch('/api/bookings', bookingBody)
+
+    console.log("new booking: ", booking.booking.bookingNumber);
+
+
+    if(booking.message.includes("Booking created!")) {
+      setToggleClassName("ticket-animation");
+      setTimeout(() => {
+        setAnimationDone(true);
+        setBookingNumber(booking.booking.bookingNumber ? booking.booking.bookingNumber : "");
+      }, 3200);
+    }
   }
 
   return (
@@ -56,12 +53,13 @@ export default function BookingConfirmation() {
             type="email"
             className="email-field"
             placeholder="example.mail@example.com"
-            name="email"
             onChange={handleBookingBody}
-            value={confirmationEmail || ''}
+            onBlur={handleBookingBody}
+            value={email || ''}
+            name="email"
             id="email">
           </input>
-          {confirmationEmail.includes('@') && (
+          {email.includes('@') && (
             <button className='send-btn' onClick={handleSendConfirmation}>Skicka bekräftelsen</button>
           )}
         </div>

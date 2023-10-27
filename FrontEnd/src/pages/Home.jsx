aba1b1e8b401e32b2ee541676003611850ccdcdc
SerialPenguin <larsson.alexander@live.se>
2023-10-27 10:31:33 +0200

    Fixed minor bugs in booking.jsx

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,5 +1,7 @@
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+/** @format */
+
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   const [screenings, setScreenings] = useState([]);
@@ -7,32 +9,35 @@ function Home() {
   useEffect(() => {
     const fetchScreenings = async () => {
       try {
-        const response = await fetch('/api/screenings');
-        if (!response.ok) throw new Error('Failed to fetch screenings');
+        const response = await fetch("/api/screenings");
+        if (!response.ok) throw new Error("Failed to fetch screenings");
         const data = await response.json();
         setScreenings(data);
       } catch (error) {
-        console.error('Error fetching screenings data:', error);
+        console.error("Error fetching screenings data:", error);
       }
     };
 
-    fetchScreenings(); 
+    fetchScreenings();
   }, []);
 
   return (
     <div className="App">
-      <h1>Screenings</h1>
+      <h1>Visningar</h1>
       <div className="screening-grid">
         {screenings.length === 0 ? (
-          <p>No screenings found</p>
+          <p>Inga visningar hittades</p>
         ) : (
           screenings.map((screening) => (
             <div key={screening._id} className="screening-card">
               <h2>{screening.movie.title}</h2>
               <p>{screening.movie.description}</p>
-              <img src={screening.movie.images[0]} alt={screening.movie.title} />
-              <Link to={`/booking/${screening._id}`}> 
-                <button>Book Now</button>
+              <img
+                src={screening.movie.images[0]}
+                alt={screening.movie.title}
+              />
+              <Link to={`/booking/${screening._id}`}>
+                <button>Boka Nu</button>
               </Link>
             </div>
           ))

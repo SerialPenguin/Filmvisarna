de444dce52289c3b8cc73138893d5d800d52a9f7
PontusEkstrom02 <113009598+PontusEkstrom02@users.noreply.github.com>
2023-10-27 10:08:39 +0200

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,17 +1,9 @@
 import './App.css';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet} from 'react-router-dom';
 
 function App() {
   return (
     <>
-    <ul>
-      <li>
-        <Link to="/">home</Link>
-      </li>
-      <li>
-        <Link to="/screenings">screenings</Link>
-      </li>
-    </ul>
       <Outlet />
     </>
   );

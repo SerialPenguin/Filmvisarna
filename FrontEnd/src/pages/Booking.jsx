209c073136ebac3adde30ae52711b5927d2d4dc7
SerialPenguin <larsson.alexander@live.se>
2023-10-30 20:10:17 +0100

    Moved out the week counter into weekUtil.js

--- FrontEnd/src/pages/Booking.jsx
+++ FrontEnd/src/pages/Booking.jsx
@@ -75,32 +75,6 @@ function Booking() {
     fetchMovies();
   }, []);
 
-  // useEffect(() => {
-  //   // Function to get the week number of a date
-  //   const getWeekNumber = (date) => {
-  //     const d = new Date(date);
-  //     d.setHours(0, 0, 0, 0);
-  //     d.setDate(d.getDate() + 4 - (d.getDay() || 7));
-  //     const yearStart = new Date(d.getFullYear(), 0, 1);
-  //     const weekNumber = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
-  //     return weekNumber;
-  //   };
-
-  //   const groupScreeningsByWeek = (screenings) => {
-  //     const grouped = {};
-  //     for (const screening of screenings) {
-  //       const week = getWeekNumber(new Date(screening.startTime));
-  //       if (!grouped[week]) {
-  //         grouped[week] = [];
-  //       }
-  //       grouped[week].push(screening);
-  //     }
-  //     return Object.keys(grouped).map((week) => ({
-  //       week,
-  //       screenings: grouped[week],
-  //     }));
-  //   };
-
   useEffect(() => {
     if (selectedMovie) {
       fetch(`/api/screenings`)

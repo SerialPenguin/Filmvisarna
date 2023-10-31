58849e1f101a10cfc5afdd7a644ddc1d9524d7be
SerialPenguin <larsson.alexander@live.se>
2023-10-31 16:07:16 +0100

    Booking CSS and removal from localStorage when a user closes the webpage

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,9 +2,21 @@
 
 import HeaderNavComponent from "./Components/HeaderNavComponent.jsx";
 import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      localStorage.removeItem("bookingData");
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
   return (
     <>
       <HeaderNavComponent />

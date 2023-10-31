71a28fcc4f127369c0ce6d4031b0951001e980f0
SerialPenguin <larsson.alexander@live.se>
2023-10-31 17:10:19 +0100

    Changed to sessionStorage instead of localStorage

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,21 +2,9 @@
 
 import HeaderNavComponent from "./Components/HeaderNavComponent.jsx";
 import { Outlet } from "react-router-dom";
-import { useEffect } from "react";
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    const handleBeforeUnload = () => {
-      localStorage.removeItem("bookingData");
-    };
-
-    window.addEventListener("beforeunload", handleBeforeUnload);
-
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, []);
   return (
     <>
       <HeaderNavComponent />

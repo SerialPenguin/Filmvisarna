0ef69fe4bc8ab81d45413b2dd829ebb621c666ea
Erica Eklund <erica@iMac-som-tillhor-Erica.local>
2023-11-03 15:01:58 +0100

    frontpage hover

--- FrontEnd/src/pages/HomePage.jsx
+++ FrontEnd/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import FooterComponent from "../components/FooterComponent";
 import FrontCarouselComponent from "../components/FrontCarouselComponent";
-import MovieCardComponent from "../components/MovieCardComponent";
+import MovieCardComponent from "../components/MovieCardComponent/MovieCardComponent";
 
 const HomePage = () => {
   return (

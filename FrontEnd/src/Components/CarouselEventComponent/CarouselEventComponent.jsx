d3189a5e936d2487f30c80f5c1c9d63279f0f16a
Erica Eklund <erica@iMac-som-tillhor-Erica.local>
2023-11-07 14:15:32 +0100

    frontpage design

--- FrontEnd/src/Components/CarouselEventComponent/CarouselEventComponent.jsx
+++ FrontEnd/src/Components/CarouselEventComponent/CarouselEventComponent.jsx
@@ -6,8 +6,13 @@ const CarouselEventComponent = () => {
   return (
       <div className='event-carousel-container'>
       <p className='event-carousel-title'>Halloween Extravaganza</p>
+      <div>
       <img src={bluehour} alt="" className='event-carousel-img'/>
       <img src={rubber} alt="" className='event-carousel-img'/>
+      </div>
+      <div>
+      <p className='event-carousel-text'>Sugen på en spöklik Halloween? Gör din kväll riktigt otäck med vårt urval av internationella skräckfilmer.</p>
+      </div>
     </div>
     
   )
--- FrontEnd/src/Components/CarouselMemberComponent/CarouselMemberComponent.jsx
+++ FrontEnd/src/Components/CarouselMemberComponent/CarouselMemberComponent.jsx
@@ -5,7 +5,7 @@ const CarouselMemberComponent = () => {
   return (
       <div className="member-carousel-container">
         <div className="member-carousel-box">
-      <h2 className="member-carousel-title">Bli ny medlem</h2>
+      <h2 className="member-carousel-title">BLI NY MEDLEM</h2>
       <p className="member-carousel-text">Som medlem får du härliga erbjudanden och andra fördelar.</p>
        <p className="member-carousel-text"> Tryck på knappen för att registrera dig!</p>
       <Link to={'/register'}>

24fc0521157def829136443a47f6f00c7918f452
SerialPenguin <larsson.alexander@live.se>
2023-11-09 12:05:55 +0100

    Final changes to booking, when closing a website, the saved seats are removed but adblock can cause issues

--- FrontEnd/src/Components/BookingConfirmationComponent/BookingConfirmation.jsx
+++ FrontEnd/src/Components/BookingConfirmationComponent/BookingConfirmation.jsx
@@ -66,6 +66,7 @@ export default function BookingConfirmation(props) {
     }
 
     fetchProfile();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [token]);
 
   useEffect(() => {

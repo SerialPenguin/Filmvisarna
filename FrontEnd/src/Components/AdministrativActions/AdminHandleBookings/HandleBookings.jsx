fb821e6ea4bb4434501c5c14f17dba81d67076e4
RobbanBroberg <lafarchetamustatche@outlook.com>
2023-11-15 16:51:34 +0100

    changes done to navcomp

--- FrontEnd/src/Components/AdministrativActions/AdminHandleBookings/HandleBookings.jsx
+++ FrontEnd/src/Components/AdministrativActions/AdminHandleBookings/HandleBookings.jsx
@@ -4,9 +4,11 @@ import { useEffect, useState } from "react";
 export default function HandleBookings() {
 
   const [query, setQuery] = useState();
+  const [key, setKey] = useState();
+  const [value, setValue] = useState();
 
   useEffect(() => {
-    setFormBody({ ...query, [key]: value });
+    setQuery({ ...query, [key]: value });
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [key, value]);
 
@@ -17,7 +19,10 @@ export default function HandleBookings() {
 
   return (
     <div>
-        <div></div>
+        <form>
+          <h4>Sök på bokningsnummer</h4>
+          <input></input>
+        </form>
     </div>
   )
 }

abdffb7f425c6a25fb2b8e6f7db0eb666985d24d
Eialist <erica@iMac-som-tillhor-Erica.local>
2023-11-21 11:46:05 +0100

    fixed some styling on big screen

--- FrontEnd/src/pages/Events.jsx
+++ FrontEnd/src/pages/Events.jsx
@@ -6,6 +6,7 @@ import "./Events.css";
 const Events = () => {
   const eventsInfo = [
     {
+      id: 1,
       date: "15 oktober - 4 november",
       movie: "The Blue Hour",
       img: blueHour,
@@ -14,6 +15,7 @@ const Events = () => {
         "Dyk in i Halloween-natten med Filmvisarna och upplev en oöverträffad skräckupplevelse. Vi presenterar två internationella skräckmästerverk som kommer få ditt hjärta att klappa snabbare. Vårt dunkla bioljus skapar den perfekta atmosfären för en oförglömlig kväll fylld med spänning och skräck. Var beredd på att bli förförd av mörkrets mystik och upplev en natt som kommer sätta sina spår i dina drömmar. Från överraskande vändningar till skrämmande scener - vågar du ge dig hän åt den rysliga njutningen? Ta mod till dig och fira Halloween med oss på Filmvisarna för en natt du sent kommer glömma.",
     },
     {
+      id: 2, 
       date: "1 december - 30 december",
       movie: "Grinchen",
       img: grinch,
@@ -22,6 +24,7 @@ const Events = () => {
         "Följ med oss på Filmvisarna in i en julfylld värld, där två hjärtvärmande internationella julsagor väntar. I vår ombonade biosalong kommer du att uppleva känslan av julefrid och överraskningar. Luta dig tillbaka och låt dessa berättelser fylla ditt hjärta med värme och glädje. Från stämningsfulla miljöer till känslosamma ögonblick, varje scen är noggrant utvald för att skapa en underbar kväll som sprider julefrid och lycka. Ta med familj och vänner för att dela denna magiska stund tillsammans. Skapa minnen och låt juleandens glädje lysa upp ditt hjärta hos oss på Filmvisarna.",
     },
     {
+      id: 3,
       date: "3 januari - 16 januari",
       movie: "Mio min Mio",
       img: mio,
@@ -36,7 +39,7 @@ const Events = () => {
       <h1 className="event-header">Event</h1>
       {/* <p className="event-text">Här kan du få information om vad som händer hos oss på Filmvisarna.</p> */}
       {eventsInfo.map((event) => (
-        <div key={event.title} className="event-cards">
+        <div key={event.title} id={event.id} className="event-cards">
           <h2 className="event-title">{event.title}</h2>
           <img className="event-img" src={event.img} alt="" />
           <div className="event-textbox">

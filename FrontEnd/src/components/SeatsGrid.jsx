ceb665daaf74bca9626735fbb47a42cb68fb9e62
SerialPenguin <larsson.alexander@live.se>
2023-11-23 15:37:39 +0100

    Working on main

---
+++ FrontEnd/src/components/SeatsGrid.jsx
@@ -0,0 +1,49 @@
+/** @format */
+import "../pages/Booking.css";
+import PropTypes from "prop-types";
+
+function SeatsGrid({
+  salonLayout,
+  isSeatBooked,
+  handleSeatClick,
+  selectedSeats,
+}) {
+  return (
+    <div className="seats-grid">
+      {salonLayout?.rows?.map((row) => (
+        <div key={row.rowNumber} className="row">
+          {row.seats?.map((seatNumber) => (
+            <button
+              id={`${seatNumber}`}
+              key={seatNumber}
+              className={`${isSeatBooked(seatNumber) ? "" : "available"} ${
+                selectedSeats.includes(seatNumber) ? "temporary-selected" : ""
+              } ${
+                isSeatBooked(seatNumber) & !selectedSeats.includes(seatNumber)
+                  ? "booked"
+                  : ""
+              } `}
+              onClick={() => handleSeatClick(row.rowNumber, seatNumber)}>
+              {seatNumber}
+            </button>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+SeatsGrid.propTypes = {
+  salonLayout: PropTypes.shape({
+    rows: PropTypes.arrayOf(
+      PropTypes.shape({
+        rowNumber: PropTypes.number.isRequired,
+        seats: PropTypes.arrayOf(PropTypes.number).isRequired,
+      })
+    ),
+  }),
+  isSeatBooked: PropTypes.func.isRequired,
+  handleSeatClick: PropTypes.func.isRequired,
+};
+
+export default SeatsGrid;

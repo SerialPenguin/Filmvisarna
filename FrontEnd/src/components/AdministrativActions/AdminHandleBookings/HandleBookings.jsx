cac4408e6ecf9f857c068275f1df3b04850a5abf
RobbanBroberg <lafarchetamustatche@outlook.com>
2023-11-23 14:46:05 +0100

    last changes

--- FrontEnd/src/components/AdministrativActions/AdminHandleBookings/HandleBookings.jsx
+++ FrontEnd/src/components/AdministrativActions/AdminHandleBookings/HandleBookings.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { authGet } from "../../../hooksAndUtils/fetchUtil";
 import { useDebounce } from "../../../hooksAndUtils/debounce";
 import './HandleBookings.css';
